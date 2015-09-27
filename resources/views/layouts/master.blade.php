38275040cb6bf5923fb9750c7f32c11218d50dbb
David Lemayian <david@on.co.ke>
2015-09-27 08:57:51 +0300

    Removed custom fonts.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,9 +10,6 @@
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <!-- Place favicon.ico in the root directory -->
 
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-
     <link rel="stylesheet" href="/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
